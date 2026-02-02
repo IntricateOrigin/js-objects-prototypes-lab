2cc6a849876a3890abf4c175e23e4012d3f6a8ac
Kyle Gladden <202115445@ub.edu.bz>
2026-02-01 22:12:15 -0600

    Complete Part 2: Arrow function Operations

---
+++ exercise2.js
@@ -0,0 +1,21 @@
+// Name: Kyle Gladden
+// Assessment: Lab#1
+
+const numbers = [1, 2, 3, 4, 5];
+
+//variable containing an squared version of the values in the numbers array
+const squared = numbers.map(num => num*num);
+
+//Variable containing even values from the array
+const evens = numbers.filter(num => num%2 == 0);
+
+//Sums each value in the array together
+const sum = numbers.reduce((total, sum) => {
+    
+    return total + sum;
+
+}, 0);
+
+console.log("Squared: ", squared);
+console.log("Even numbers: ", evens);
+console.log("Sum: ", sum());
